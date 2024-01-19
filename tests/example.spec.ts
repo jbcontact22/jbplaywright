af8c03d260734037912c26addf87886eed4995ad
jbcontact22 <jackbuckner@austin.rr.com>
2024-01-19 07:49:55 -0600

    Updated tests working on ASP app.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,27 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
+// These are customized 
 test('jb has title', async ({ page }) => {
   await page.goto('http://localhost:5067/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Home Page/);
+  await expect(page).toHaveTitle(/Home page - jbplayapp/);
 });
 
 test('jb microsoft link', async ({ page }) => {
@@ -29,7 +13,14 @@ test('jb microsoft link', async ({ page }) => {
 
   // Click the get started link.
   await page.getByRole('link', { name: 'building Web apps with ASP.NET Core' }).click();
+});
+
+test('check privacy policy', async ({ page }) => {
+  await page.goto('http://localhost:5067/');
+
+  // Click the get started link.
+  await page.locator('Privacy').click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Privacy Policy' })).toBeVisible();
 });
