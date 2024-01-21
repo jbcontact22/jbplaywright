a426b331fbb9eddf8d85f3277e618c1db41288dc
jbcontact22 <jackbuckner@austin.rr.com>
2024-01-21 16:37:52 -0600

    Now auto launch the web server and use relative path in tests.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,24 +2,24 @@ import { test, expect } from '@playwright/test';
 
 // These are customized 
 test('jb has title', async ({ page }) => {
-  await page.goto('http://localhost:5067/');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Home page - jbplayapp/);
 });
 
 test('jb microsoft link', async ({ page }) => {
-  await page.goto('http://localhost:5067/');
+  await page.goto('/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'building Web apps with ASP.NET Core' }).click();
 });
 
 test('check privacy policy', async ({ page }) => {
-  await page.goto('http://localhost:5067/');
+  await page.goto('/');
 
   // Click the get started link.
-  await page.locator('Privacy').click();
+  await page.locator('.navbar-collapse >> text=Privacy').click();
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Privacy Policy' })).toBeVisible();
